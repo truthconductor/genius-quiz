64d003f038922a7166c207957904d7de9a808627
truthconductor <truthconductor@gmail.com>
2019-08-26 23:24:59 +0900

    remove quiz has questions inverse_of

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,5 +1,5 @@
 class Quiz < ApplicationRecord
   belongs_to :user
-  has_many :questions, ->{ order(:order) }, inverse_of: :quiz, dependent: :destroy
+  has_many :questions, ->{ order(:order) }, dependent: :destroy
   accepts_nested_attributes_for :questions
 end
