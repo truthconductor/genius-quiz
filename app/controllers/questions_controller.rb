24793e4eb2c8d5817c3a1dc644285d54258ee890
truthconductor <truthconductor@gmail.com>
2019-08-29 23:20:19 +0900

    add quiz/question challenge and result count increment

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,11 +8,38 @@ class QuestionsController < ApplicationController
     user_answer = answer_params[:answer] == "true" ? true : false
     @question = Question.find(answer_params[:id])
 
+    #クイズ作成者でない回答か確認
+    isChallenger = @question.quiz.user.id != current_user&.id
+
+    #正解か
     @is_correct = user_answer == @question.is_yes
 
     if @is_correct
       @next_question = Question.where(quiz_id: @question.quiz_id, order: @question.order+1).first
+      #データベース更新
+      if isChallenger
+        #出題回数正解回数更新
+        @question.total_answer_counts = @question.total_answer_counts+1
+        @question.correct_answer_counts = @question.correct_answer_counts+1
+        @question.save
+        #最終問題正解
+        unless @next_question
+          quiz = @question.quiz
+          quiz.question_times = quiz.question_times + 1
+          quiz.genius_times = quiz.genius_times + 1
+          quiz.save
+        end
+      end
     else
+      if isChallenger
+        #出題回数正解回数更新
+        @question.total_answer_counts = @question.total_answer_counts+1
+        @question.save
+        #出題回数更新
+        quiz = @question.quiz
+        quiz.question_times = quiz.question_times + 1
+        quiz.save
+      end
     end
 
   end
