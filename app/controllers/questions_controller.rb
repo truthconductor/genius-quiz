664ce967f3bc5f0773a2a48fea2d4454015fc063
truthconductor <truthconductor@gmail.com>
2019-08-31 01:27:10 +0900

    add database lock and transaction

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,42 +6,48 @@ class QuestionsController < ApplicationController
   def answer
     #解答チェック
     user_answer = answer_params[:answer] == "true" ? true : false
-    @question = Question.find(answer_params[:id])
 
-    #クイズ作成者でない回答か確認
-    isChallenger = @question.quiz.user.id != current_user&.id
+    #悲観的ロック
+    Question.transaction do
+      #問題
+      @question = Question.find(answer_params[:id])
+      #クイズ作成者か判断
+      isChallenger = @question.quiz.user.id != current_user&.id
+      #問題が正解か判断
+      @is_correct = user_answer == @question.is_yes
 
-    #正解か
-    @is_correct = user_answer == @question.is_yes
-
-    if @is_correct
-      @next_question = Question.where(quiz_id: @question.quiz_id, order: @question.order+1).first
-      #データベース更新
-      if isChallenger
-        #出題回数正解回数更新
-        @question.total_answer_counts = @question.total_answer_counts+1
-        @question.correct_answer_counts = @question.correct_answer_counts+1
-        @question.save
-        #最終問題正解
-        unless @next_question
+      #正解
+      if @is_correct
+        #次の問題を取得
+        @next_question = Question.where(quiz_id: @question.quiz_id, order: @question.order+1).first
+        #データベース更新
+        if isChallenger
+          #出題回数正解回数更新
+          @question.total_answer_counts = @question.total_answer_counts+1
+          @question.correct_answer_counts = @question.correct_answer_counts+1
+          @question.save
+          #最終問題正解（次の問題が無い）
+          unless @next_question
+            #クイズ通算成績更新
+            quiz = @question.quiz
+            quiz.question_times = quiz.question_times + 1
+            quiz.genius_times = quiz.genius_times + 1
+            quiz.save
+          end
+        end
+      #不正解
+      else
+        if isChallenger
+          #出題回数正解回数更新
+          @question.total_answer_counts = @question.total_answer_counts+1
+          @question.save
+          #クイズ通算成績更新
           quiz = @question.quiz
           quiz.question_times = quiz.question_times + 1
-          quiz.genius_times = quiz.genius_times + 1
           quiz.save
         end
       end
-    else
-      if isChallenger
-        #出題回数正解回数更新
-        @question.total_answer_counts = @question.total_answer_counts+1
-        @question.save
-        #出題回数更新
-        quiz = @question.quiz
-        quiz.question_times = quiz.question_times + 1
-        quiz.save
-      end
     end
-
   end
 
 
