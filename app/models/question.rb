e8017126e1a5b3b34d43c1364ad5308c047b2d78
truthconductor <truthconductor@gmail.com>
2019-08-31 13:00:33 +0900

    add question validation and show errormessage

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,9 @@
 class Question < ApplicationRecord
+
+  #バリデーション
+  validates :sentence, presence: true, length: { maximum: 200 }
+
+  #アソシエーション
   belongs_to :quiz, inverse_of: :questions
   validates_presence_of :quiz
 end
