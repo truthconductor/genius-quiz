7d4f2c05d04793ce7cb46120a3073799a310eaf5
truthconductor <truthconductor@gmail.com>
2019-08-31 14:19:19 +0900

    add question validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,8 @@ class Question < ApplicationRecord
 
   #バリデーション
   validates :sentence, presence: true, length: { maximum: 200 }
+  validates :is_yes, presence: true
+  validates :explanation, length: { maximum: 200 }
 
   #アソシエーション
   belongs_to :quiz, inverse_of: :questions
